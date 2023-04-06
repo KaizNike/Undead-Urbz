fca479835bc0553a9331539b3e200333f025e099
KaizNike <62921588+KaizNike@users.noreply.github.com>
2023-04-06 00:53:19 -0500

    Add Actor Node and Player data in UI

---
+++ Undead Urbz/Actor.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var isHuman = true # Set false if zombie
+var isAlive = true # Set false if killed
+
+var inv = []
+var item = {"Type": "", "Uses": "", "hasUses": false}
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
